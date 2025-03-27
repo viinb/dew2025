ba47752dc4259172b36ca2cc9f6f5971e0635897
vrodrg <vic.rodrg@gmail.com>
2025-03-27 07:43:01 -0300

    mudanças

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 /**
+ * Série de scripts que deixam varias coisas mais práticas
+ */
+
+/**
  * Economiza tempo... e troca a página
+ * @param {string} nome 
+ * @param {string} pasta 
  */
 function irParaPagina(nome, pasta) {
     window.location.href = pasta + "/" + nome + ".html";
